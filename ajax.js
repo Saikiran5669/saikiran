38a0adb5abedeb72f93cc3e9f4802c3a27576811
saikiran <kirancj5669@gmail.com>
2019-03-02 17:38:42 -0600

    up

--- ajax.js
+++ ajax.js
@@ -1,10 +1,21 @@
-function ajax() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById(aja).innerHTML = this.responseText;
-      }
-    };
-    xhttp.open("GET", "ajax.txt", true);
-    xhttp.send();
+// function ajax() {
+//     var xhttp = new XMLHttpRequest();
+//     xhttp.onreadystatechange = function() {
+//       if (this.readyState == 4 && this.status == 200) {
+//         document.getElementById(aja).innerHTML = this.responseText;
+//       }
+//     };
+//     xhttp.open("GET", "./ajax.txt", true);
+//     xhttp.send();
+//   }
+
+function ajax_example() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+  document.getElementById("ajax_demo").innerHTML = this.responseText;
+  }
+  };
+  xhttp.open("GET", "./ajax.txt", true);
+  xhttp.send();
   }
